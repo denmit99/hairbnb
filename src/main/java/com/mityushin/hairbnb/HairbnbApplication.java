0b2af7f02cab3c8dfe83364db140e8753b7e3446
Денис Митюшин <d.mityushin@dnx.team>
2023-08-28 19:23:15 +0700

    Init commit

---
+++ src/main/java/com/mityushin/hairbnb/HairbnbApplication.java
@@ -0,0 +1,13 @@
+package com.mityushin.hairbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairbnbApplication.class, args);
+	}
+
+}
